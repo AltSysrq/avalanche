b45e55217071f887eb448e4cec61457492a9b9ef
Jason Lingle <jason@lin.gl>
2015-04-18 12:49:31 -0600

    Support random string combination in rope visualiser.

--- test/runtime/rope-vis.c
+++ test/runtime/rope-vis.c
@@ -36,92 +36,105 @@
 #include "runtime/avalanche.h"
 #include "bsd.h"
 
-static void make_strings(ava_string*, unsigned);
-static void write_graph(ava_string*, unsigned);
+static void make_strings(ava_string*, unsigned, unsigned);
+static void write_graph(ava_string*, unsigned, unsigned);
 static void write_tree(FILE*, ava_rope*, int is_live);
 static void unmark_tree(ava_rope*);
 static int is_marked(ava_rope*);
 static void set_mark(ava_rope*, int);
 
 int main(int argc, char** argv) {
-  if (2 != argc)
-    errx(EX_USAGE, "Usage: %s <num-iterations>", argv[0]);
+  if (3 != argc)
+    errx(EX_USAGE, "Usage: %s <num-iterations> <stride>", argv[0]);
+
+  srand(0);
 
   unsigned count = atoi(argv[1]);
-  ava_string strings[count];
-  make_strings(strings, count);
+  unsigned stride = atoi(argv[2]);
+  ava_string strings[count*count];
+  memset(strings, 0, sizeof(strings));
+  make_strings(strings, count, stride);
 
   unsigned i;
-  for (i = 2; i < count; ++i)
-    write_graph(strings, i);
+  for (i = 0; i < count; ++i)
+    write_graph(strings, count, i);
 }
 
-static void make_strings(ava_string* dst, unsigned count) {
+static void make_strings(ava_string* dst, unsigned count, unsigned stride) {
   char buf[128];
-  ava_string base;
-  unsigned i;
+  unsigned i, t, merge;
 
   memset(buf, 'a', sizeof(buf));
-  base = ava_string_of_bytes(buf, sizeof(buf));
 
-  dst[0] = AVA_EMPTY_STRING;
-  for (i = 1; i < count; ++i)
-    dst[i] = ava_string_concat(dst[i-1], base);
+  for (i = 0; i < count; ++i)
+    dst[i] = ava_string_of_bytes(buf, sizeof(buf));
+
+  for (t = 1; t < count; ++t) {
+    merge = rand() % (stride < count - t? stride : count - t);
+    for (i = 0; i < merge; ++i)
+      dst[t*count + i] = dst[(t-1)*count + i];
+
+    dst[t*count + merge] = ava_string_concat(dst[(t-1)*count + merge],
+                                             dst[(t-1)*count + merge+1]);
+
+    for (i = merge+1; i < count - t; ++i)
+      dst[t*count + i] = dst[(t-1)*count + i+1];
+  }
 }
 
-static void write_graph(ava_string* strings, unsigned n) {
+static void write_graph(ava_string* strings, unsigned count, unsigned tmax) {
   char filename[16];
   FILE* out;
-  unsigned i;
+  unsigned i, t;
 
-  snprintf(filename, sizeof(filename), "%03d.dot", n);
+  snprintf(filename, sizeof(filename), "%03d.dot", tmax);
   out = fopen(filename, "w");
   fprintf(out, "digraph {\n");
 
-  for (i = n - 1; i > 0; --i)
-    write_tree(out, (ava_rope*)strings[i].rope, i == n - 1);
+  for (t = tmax; t <= tmax; --t) {
+    for (i = 0; i < count; ++i) {
+      if (strings[t*count + i].rope)
+        write_tree(out, (ava_rope*)strings[t*count + i].rope, t == tmax);
+    }
+  }
 
-  for (i = n - 1; i > 0; --i)
-    unmark_tree((ava_rope*)strings[i].rope);
+  for (t = 0; t <= tmax; ++t)
+    for (i = 0; i < count; ++i)
+      if (strings[t*count + i].rope)
+        unmark_tree((ava_rope*)strings[t*count + i].rope);
 
   fprintf(out, "}\n");
   fclose(out);
 }
 
 static void write_tree(FILE* out, ava_rope* rope, int is_live) {
-  if (!rope->depth || is_marked(rope)) return;
+  if (is_marked(rope)) return;
 
   fprintf(out, "  \"%p\" [style=filled,fillcolor=%s,label=\"%d\"];\n",
-          rope, is_live? "aquamarine" : "gold", rope->depth);
+          rope, 0 == rope->depth? "azure" : is_live? "aquamarine" : "gold",
+          rope->depth);
 
-  if (rope->v.concat_left->depth) {
+  if (rope->depth) {
     fprintf(out, "  \"%p\" -> \"%p\";\n", rope, (void*)rope->v.concat_left);
-  } else {
-    fprintf(out, "  \"%p-lleaf\" [style=filled,fillcolor=azure,label=\"0\"];\n",
-            rope);
-    fprintf(out, "  \"%p\" -> \"%p-lleaf\";\n", rope, rope);
-  }
-
-  if (rope->concat_right->depth) {
     fprintf(out, "  \"%p\" -> \"%p\";\n", rope, (void*)rope->concat_right);
-  } else {
-    fprintf(out, "  \"%p-rleaf\" [style=filled,fillcolor=azure,label=\"0\"];\n",
-            rope);
-    fprintf(out, "  \"%p\" -> \"%p-rleaf\";\n", rope, rope);
   }
 
   set_mark(rope, 1);
 
-  write_tree(out, (ava_rope*)rope->v.concat_left, is_live);
-  write_tree(out, (ava_rope*)rope->concat_right, is_live);
+  if (rope->depth) {
+    write_tree(out, (ava_rope*)rope->v.concat_left, is_live);
+    write_tree(out, (ava_rope*)rope->concat_right, is_live);
+  }
 }
 
 static void unmark_tree(ava_rope* rope) {
   if (!is_marked(rope)) return;
 
   set_mark(rope, 0);
-  unmark_tree((ava_rope*)rope->v.concat_left);
-  unmark_tree((ava_rope*)rope->concat_right);
+  if (rope->depth) {
+    unmark_tree((ava_rope*)rope->v.concat_left);
+    unmark_tree((ava_rope*)rope->concat_right);
+  }
 }
 
 static void set_mark(ava_rope* rope, int mark) {
