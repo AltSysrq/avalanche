88248d1947d4d917551535c817da38be5313dd88
Jason Lingle <jason@lin.gl>
2015-10-11 22:21:59 -0600

    Make clang's error output more palatable.
    Since we're stuck with it for now, I guess.

--- freebsd-configure-gcc5.sh
+++ freebsd-configure-gcc5.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 
 CC=gcc5
-# TODO: Figure out how to get this to work correctly with g++5
-# For some reason it doesn't seem to link against the correct C++ stdlib.
 CXX=clang++
 COMMON_FLAGS='-Wall -Wextra -Wno-long-long -Wno-unused-parameter -g3'
 CFLAGS="$COMMON_FLAGS -Wno-clobbered  -fdiagnostics-color=never -fno-diagnostics-show-caret"
-CXXFLAGS="$COMMON_FLAGS"
+CXXFLAGS="$COMMON_FLAGS -fno-caret-diagnostics -fno-diagnostics-fixit-info -fno-color-diagnostics"
 CPPFLAGS='-I/usr/local/include'
 LDFLAGS='-L/usr/local/lib -L/usr/local/lib/gcc5'
 CXXLDFLAGS="$LDFLAGS"
